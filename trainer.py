c019f2bfba8da4c6c57f87bb39fda288ae065dbf
Jakub Belicki <jakub.belicki@brainscan.ai>
2025-03-31 13:35:22 +0200

    main clause

--- trainer.py
+++ trainer.py
@@ -3,15 +3,15 @@ from pytorch_lightning.callbacks import ModelCheckpoint
 from transformer import SmallGPT2
 import torch
 
+if __name__ == "__main__":
+    torch.set_float32_matmul_precision('medium')
 
-torch.set_float32_matmul_precision('medium')
-
-checkpoint_callback = ModelCheckpoint(
-    dirpath="checkpoints",
-    filename="bach_gen_{epoch:02d}_{loss:.2f}",
-    every_n_train_steps=100,
-    save_top_k=1,
-)
-model = SmallGPT2()
-trainer = Trainer(accelerator="gpu", callbacks=[checkpoint_callback], max_epochs=3)
-trainer.fit(model)
+    checkpoint_callback = ModelCheckpoint(
+        dirpath="checkpoints",
+        filename="bach_gen_{epoch:02d}_{loss:.2f}",
+        every_n_train_steps=100,
+        save_top_k=1,
+    )
+    model = SmallGPT2()
+    trainer = Trainer(accelerator="gpu", callbacks=[checkpoint_callback], max_epochs=3)
+    trainer.fit(model)
