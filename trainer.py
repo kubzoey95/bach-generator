0694a122e9ebe915f3791a90b373029ff9e89b76
Jakub Belicki <jakub.belicki@brainscan.ai>
2025-03-31 14:05:35 +0200

    /content/drive/MyDrive/bachgencheckpoints

--- trainer.py
+++ trainer.py
@@ -7,7 +7,7 @@ if __name__ == "__main__":
     torch.set_float32_matmul_precision('medium')
 
     checkpoint_callback = ModelCheckpoint(
-        dirpath="checkpoints",
+        dirpath="/content/drive/MyDrive/bachgencheckpoints",
         filename="bach_gen_{epoch:02d}_{loss:.2f}",
         every_n_train_steps=100,
         save_top_k=1,
